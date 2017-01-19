796e57deee0c237d82528c6fc27b376a87d577a3
samir tabriz <samirmanafzade@gmail.com>
2017-01-19 13:24:25 -0800

    HTTP Response added

--- server.c
+++ server.c
@@ -64,17 +64,8 @@ int main(int argc, char *argv[])
    	 //reply to client
 	 char *response = 
 	"HTTP/1.1 200 OK\n"
-	"Date: Thu, 19 Feb 2009 12:27:04 GMT\n"
-	"Server: Apache/2.2.3\n"
-	"Last-Modified: Wed, 18 Jun 2003 16:05:58 GMT\n"
-	"ETag: \"56d-9989200-1132c580\"\n"
-	"Content-Type: text/html\n"
-	"Content-Length: 15\n"
-	"Accept-Ranges: bytes\n"
-	"Connection: close\n"
 	"\n"
-	"sdfkjsdnbfkjbsf"; 
-
+	"samir";
    	 n = write(newsockfd,response,strlen(response));
    	 if (n < 0) error("ERROR writing to socket");
          
