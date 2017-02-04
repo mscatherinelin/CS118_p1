0a8869d261bad44fb19959f42af4e62e7ceda011
samir tabriz <samirmanafzade@gmail.com>
2017-02-03 20:47:34 -0800

    last1

--- server.c
+++ server.c
@@ -146,7 +146,7 @@ int main(int argc, char *argv[])
 	n = read(newsockfd,buffer,255);
 	if (n < 0) error("ERROR reading from socket");
 	
-	printf("Here is the message: %s\n",buffer);
+	printf("%s\n",buffer);
 
 	// get filename 
 
